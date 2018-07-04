5fe7edef223f5ae128bd184656a07eb778cbd2e6
cclovel <597413318@qq.com>
2018-07-04 16:52:46 +0800

    Create DataFormatException.java

---
+++ src/main/opentsdb/client/builder/DataFormatException.java
@@ -0,0 +1,14 @@
+package com.ygsoft.opentsdb.client.builder;
+
+public class DataFormatException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public DataFormatException(){
+		super();
+	}
+	
+	public DataFormatException(String s){
+		super();
+	}
+}
