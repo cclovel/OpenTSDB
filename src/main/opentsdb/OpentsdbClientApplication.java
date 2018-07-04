5ba9896c9b71acedcdd1b1c64e75e8166601a2bb
cclovel <597413318@qq.com>
2018-07-04 17:02:36 +0800

    Create OpentsdbClientApplication.java

---
+++ src/main/opentsdb/OpentsdbClientApplication.java
@@ -0,0 +1,12 @@
+package com.ygsoft.opentsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentsdbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentsdbClientApplication.class, args);
+	}
+}
