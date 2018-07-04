ad163cafa4cbb7047be3848396c113f18f8b9ce3
cclovel <597413318@qq.com>
2018-07-04 17:06:48 +0800

    Create ImportDateDTO.java

---
+++ src/main/opentsdb/server/dto/ImportDateDTO.java
@@ -0,0 +1,25 @@
+package com.ygsoft.opentsdb.server.dto;
+
+/**
+ * Created by yinlilan on 2018/1/19.
+ */
+public class ImportDateDTO {
+	private Object data;
+    private String apiKey;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+}
